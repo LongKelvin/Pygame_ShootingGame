397435e99cb8a030b7d9990bfaf698ffb6de2aa6
Long Kelvin <longkelvin101099@gmail.com>
2021-05-10 13:42:19 +0700

    handle key events

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 if __name__ == '__main__':
     WIDTH = 800
     HEIGHT = 600
-    FPS = 30
+    FPS = 60
 
     # define color
     WHITE = (255, 255, 255)
@@ -19,34 +19,40 @@ if __name__ == '__main__':
     game_folder = os.path.dirname(__file__)
     img_folder = os.path.join(game_folder, "img")
 
-    # Game object
-    class Player(pygame.sprite.Sprite):
-        # sprite for the player
-        def __init__(self):
-            pygame.sprite.Sprite.__init__(self)
-            self.image = pygame.image.load(os.path.join(img_folder, "p1_jump.png")).convert()
-            self.image.set_colorkey(BLACK)
-            self.rect = self.image.get_rect()
-            self.rect.center = (WIDTH / 2, HEIGHT / 2)
-            self.y_speed = 5
-
-        def update(self):
-            self.rect.x += 5
-            self.rect.y += self.y_speed
-            if self.rect.bottom > HEIGHT - 200:
-                self.y_speed = -5
-            if self.rect.top < 200:
-                self.y_speed = 5
-            if self.rect.left > WIDTH:
-                self.rect.right = 0
-
-
     # init pygame and create window
     pygame.init()
     pygame.mixer.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Shooting_Game")
     clock = pygame.time.Clock()
+
+
+    # Game object
+    class Player(pygame.sprite.Sprite):
+        # sprite for the player
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.Surface((50, 40))
+            self.image.fill(GREEN)
+            self.image.set_colorkey(BLACK)
+            self.rect = self.image.get_rect()
+            self.rect.centerx = (WIDTH / 2)
+            self.rect.bottom = (HEIGHT - 10)
+            self.speedx = 0
+
+        def update(self):
+            self.speedx = 0
+            keystate = pygame.key.get_pressed()
+            if keystate[pygame.K_LEFT]:
+                self.speedx = -5
+            if keystate[pygame.K_RIGHT]:
+                self.speedx = 5
+            self.rect.x += self.speedx
+            if self.rect.right > WIDTH:
+                self.rect.right = WIDTH
+            if self.rect.left < 0:
+                self.rect.left = 0
+
 
     all_sprites = pygame.sprite.Group()
     player = Player()
@@ -66,7 +72,7 @@ if __name__ == '__main__':
         all_sprites.update()
 
         # Draw/ Render
-        screen.fill(BLUE)
+        screen.fill(BLACK)
         all_sprites.draw(screen)
         # draw buffer
         pygame.display.flip()
