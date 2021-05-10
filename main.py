b0725c91d2cd688edd380659d0825a9e064baeed
Long Kelvin <longkelvin101099@gmail.com>
2021-05-10 13:10:35 +0700

    add game sprite

--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import pygame
 
 if __name__ == '__main__':
-    WIDTH = 360
-    HEIGHT = 480
+    WIDTH = 800
+    HEIGHT = 600
     FPS = 30
 
     # define color
@@ -13,16 +13,39 @@ if __name__ == '__main__':
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
+
+
+    # Game object
+    class Player(pygame.sprite.Sprite):
+        # sprite for the player
+        def __init__(self):
+            pygame.sprite.Sprite.__init__(self)
+            self.image = pygame.Surface((50, 50))
+            self.image.fill(GREEN)
+            self.rect = self.image.get_rect()
+            self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+        def update(self):
+            self.rect.x += 5
+            if self.rect.left > WIDTH:
+                self.rect.right = 0
+
+
     # init pygame and create window
     pygame.init()
     pygame.mixer.init()
-    screen = pygame.display.set_mode(WIDTH, HEIGHT)
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Shooting_Game")
     clock = pygame.time.Clock()
 
+    all_sprites = pygame.sprite.Group()
+    player = Player()
+    all_sprites.add(player)
     # Game loop
     running = True
     while running:
+        # keep loop running at the right speed
+        clock.tick(FPS)
         # Process input (event)
         for event in pygame.event.get():
             # check quit game
@@ -30,8 +53,12 @@ if __name__ == '__main__':
                 running = False
 
         # Update
-        # Render
-        screen.fill(BLACK)
+        all_sprites.update()
 
+        # Draw/ Render
+        screen.fill(BLACK)
+        all_sprites.draw(screen)
         # draw buffer
         pygame.display.flip()
+
+    pygame.quit()
